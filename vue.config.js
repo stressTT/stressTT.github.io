8e7cc11ef9fb4e7a55c1c0fe180974ab59a9d6c9
stressTT <batyrkhanb@bk.ru>
2023-01-11 10:54:54 +0600

    1

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/https://stresstt.github.io/'
+  publicPath: '/https://stresstt.github.io-master/'
 })
